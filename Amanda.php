431c7c5c71252b30800a45f01a4da93d0ee21e81
Miss. AA Mgijima <u13141962@up.ac.za>
2018-03-22 14:08:44 +0200

    This is my first commit!

---
+++ Amanda.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title> INF354 Homework 1 - u15185142 </title>
+	</head>
+
+	<body>
+		<form action="myProfile.php" method="post">
+		
+			<label for="firstName">First Name </label>
+			<input type="text" name="firstName" id="firstName" />
+			</br></br>
+
+			<label for="lastName">Last Name </label>
+			<input type="text" name="lastName" id="lastName" />
+			</br></br>
+
+			<label for="dob">Date of Birth </label>
+			<input type="date" name="dob" id="dob" />
+			</br></br>
+			
+			<input type="submit" />			
+
+		</form>
+	</body>
+
+</html>
